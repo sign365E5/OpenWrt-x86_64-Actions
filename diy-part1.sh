01b90dc0bfe0ac3480669fb168b2fd24a25deb0d
sign365E5 <bluejack911@126.com>
2024-09-06 06:37:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,6 @@ curl https://raw.githubusercontent.com/immortalwrt/immortalwrt/openwrt-23.05/fee
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git fanx https://github.com/FanxJK/openwrt-packages' >>feeds.conf.default
+#echo 'src-git fanx https://github.com/FanxJK/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
